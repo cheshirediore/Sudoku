a4ce197cdfdbcfb70b6ac75472d6cf722c418de9
Cheshire <cheshirediore@gmail.com>
2025-10-18 14:25:14 -0400

    Cleanup of Cell class

--- Sudoku/Cell.cs
+++ Sudoku/Cell.cs
@@ -1,42 +1,33 @@
 namespace Sudoku
 {
-    public class Cell : IEquatable<Cell>
+    public class Cell(int x, int y, int cellValue) : IEquatable<Cell>
     {
-        // "attribute"
-        int[] _coordinates;
-        // "property"
+        int[] _coordinates = new int[2] { x, y };
+        int _value = cellValue;
+
+
+        // Accessors for the X and Y coordinates
         public int X {
             get => this._coordinates[0];
             set => this._coordinates[0] = value;
         }
-        // "property"
+
         public int Y {
             get => this._coordinates[1];
             set => this._coordinates[1] = value;
         }
 
-        // "attribute"
-        int _value;
-        // "property"
-        public int Value {
+        // Accessor for the cell's value
+        public int Value
+        {
             get => this._value;
             set => this._value = value;
         }
 
         #region Constructors
-        public Cell(int x, int y, int cellValue) 
-        {
-            this._coordinates = new int[2] {x, y};
-            this._value = cellValue;
-        }
+        public Cell(int x, int y) : this(x, y, 0) { }
 
-        public Cell(int x, int y) : this(x, y, 0)
-        {
-        }
-
-        public Cell() : this(0, 0)
-        {
-        }
+        public Cell() : this(0, 0) { }
         #endregion
 
         #region InterfaceImplementation
