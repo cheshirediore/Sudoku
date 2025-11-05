c59e1c84d63db80bd8cb84de2fabee4bbae36b36
Cheshire <cheshirediore@gmail.com>
2025-11-05 14:31:32 -0500

    Draft Backtracker and Grid classes to abstract out the related systems. The goal with this is to make the generator algorithm cleaner and easier to write.

---
+++ Sudoku/Backtracker.cs
@@ -0,0 +1,23 @@
+namespace Sudoku;
+
+public class Backtracker
+{
+
+    // int[] root;
+
+    // int[][] solutions;
+
+
+    // From wikipedia:
+    // root(P): return the partial candidate at the root of the search tree
+
+    // reject(P, c): return true only if the partial candidate c is not worth completing
+
+    // accept(P, c): return true if and only if candidate c is a solution of P
+
+    // first(P, c): gnerate the first extension of candidate c
+
+    // next(P, s): generate the next extension of a candidate after the extension s.
+
+    // output(P, c): use the solution c of P, as appropriate to the application
+}
---
+++ Sudoku/Grid.cs
@@ -0,0 +1,186 @@
+using System.Text;
+
+namespace Sudoku;
+
+public class Grid
+{
+    public readonly int[,] vertices;
+
+    public Grid()
+    {
+        vertices = new int[9, 9];
+
+        // Debug dummy values
+        for (int y = 0; y < 9; y++)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                vertices[y, x] = x;
+            }
+        }
+    }
+
+    public static int[] IndexToCoordinates(int index, int width)
+    {
+        int[] coordinates = [-1, -1];
+
+        // x = (index % width)
+        coordinates[0] = index % width;
+        // y = floor(index / width)
+        coordinates[1] = (int)Math.Floor((double)(index / width));
+
+        return coordinates;
+    }
+    
+    public int GetVertex(int x, int y)
+    {
+        return vertices[y, x];
+    }
+
+    public int GetVertex(int index)
+    {
+        int[] coords = IndexToCoordinates(index, 9);
+        return GetVertex(coords[0], coords[1]);
+    }
+
+    public void SetVertex(int index, int value)
+    {
+        int[] coordinates = IndexToCoordinates(index, 9);
+        int x = coordinates[0];
+        int y = coordinates[1];
+
+        vertices[y, x] = value;
+    }
+
+    public int[] GetRow(int rowIndex)
+    {
+        int[] row = new int[9];
+        for (int x = 0; x < 9; x++)
+        {
+            row[x] = GetVertex(x, rowIndex);
+        }
+        return row;
+    }
+
+    public int[] GetColumn(int columnIndex)
+    {
+        // TODO: Handle out of bounds arguments
+        int[] column = new int[9];
+        for (int y = 0; y < 9; y++)
+        {
+            column[y] = GetVertex(columnIndex, y);
+        }
+        return column;
+    }
+
+    public int[] GetBlock(int blockIndex)
+    {
+        // TODO: Handle out of bounds arguments
+        int[] block = new int[9];
+
+        /*
+            A block is the intersection of (union of three columns) and (union of three rows)
+
+            Block 0
+            (columns 0, 1, 2) intersect (rows 0, 1, 2)
+            Block 1
+            (columns 3, 4, 5) intersect (rows 0, 1, 2)
+            Block 2
+            (columns 6, 7, 8) intersect (rows 0, 1, 2)
+
+            Block 3
+            (columns 0, 1, 2) intersect (rows 3, 4, 5)
+            Block 4
+            (columns 3, 4, 5) intersect (rows 3, 4, 5)
+            Block 5
+            (columns 6, 7, 8) intersect (rows 3, 4, 5)
+
+            Block 6
+            (columns 0, 1, 2) intersect (rows 6, 7, 8)
+            Block 7
+            (columns 3, 4, 5) intersect (rows 6, 7, 8)
+            Block 8
+            (columns 6, 7, 8) intersect (rows 6, 7, 8)
+        */
+
+        int[] columnIndices = new int[3];
+        int[] rowIndices = new int[3];
+
+        // Give useful names to the index groups
+        int[] FIRST = [0, 1, 2];
+        int[] SECOND = [3, 4, 5];
+        int[] THIRD = [6, 7, 8];
+
+        // Only 9x9 grids are supported, so we can hard code these cases instead of calculating them at runtime
+        switch (blockIndex)
+        {
+            // Top row of blocks
+            case 0:
+                columnIndices = FIRST;
+                rowIndices = FIRST;
+                break;
+            case 1:
+                columnIndices = SECOND;
+                rowIndices = FIRST;
+                break;
+            case 2:
+                columnIndices = THIRD;
+                rowIndices = FIRST;
+                break;
+            // Middle row of blocks
+            case 3:
+                columnIndices = FIRST;
+                rowIndices = SECOND;
+                break;
+            case 4:
+                columnIndices = SECOND;
+                rowIndices = SECOND;
+                break;
+            case 5:
+                columnIndices = THIRD;
+                rowIndices = SECOND;
+                break;
+            // Bottom row of blocks
+            case 6:
+                columnIndices = FIRST;
+                rowIndices = THIRD;
+                break;
+            case 7:
+                columnIndices = SECOND;
+                rowIndices = THIRD;
+                break;
+            case 8:
+                columnIndices = THIRD;
+                rowIndices = THIRD;
+                break;
+        }
+
+        // Iterate through the indices to get the cells in the block, as defined above
+        int i = 0;
+        for (int columnIndex = 0; columnIndex < columnIndices.Length; columnIndex++)
+        {
+            for (int rowIndex = 0; rowIndex < rowIndices.Length; rowIndex++)
+            {
+                block[i] = GetVertex(columnIndices[columnIndex], rowIndices[rowIndex]);
+                i++;
+            }
+        }
+        return block;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new();
+
+        for (int y = 0; y < 9; y++)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                builder.Append($"{vertices[y, x]} ");
+            }
+            builder.AppendLine();
+        }
+
+        return builder.ToString();
+    }
+}
